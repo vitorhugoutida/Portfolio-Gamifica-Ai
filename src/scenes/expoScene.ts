c969e4905623e3acac53d9902ad8d5dbe6474f7d
vitorhugoutida <vitorhugoutida@gmail.com>
2024-06-10 17:28:09 -0300

    feat: Interligou o mapa

--- src/scenes/expoScene.ts
+++ src/scenes/expoScene.ts
@@ -1,4 +1,5 @@
-import { Color, FadeInOut, Scene, Transition } from "excalibur";
+import { Color, Engine, FadeInOut, Resource, Scene, Transition } from "excalibur";
+import { Resources } from "../resources";
 
 export class expoScene extends Scene {
 
@@ -8,6 +9,15 @@ export class expoScene extends Scene {
             color: Color.Black,
             duration: 1000
         })
+    }
+
+    onInitialize(engine: Engine<any>): void {
+        // Carregar o mapa
+        let tiledMap = Resources.Mapa
+
+        // Adicionar o mapa na cena 
+
+        tiledMap.addToScene(this)
     }
 
     // elementoHTML?: HTMLElement
