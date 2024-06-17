cb13e6fa4b11d3d82dce5c92d31c23d71b3327c1
vitorhugoutida <vitorhugoutida@gmail.com>
2024-06-17 20:52:27 -0300

    feat Exercicio nao finalizado, sobre 3 mesas

--- src/scenes/caseScene.ts
+++ src/scenes/caseScene.ts
@@ -20,7 +20,37 @@ export class caseScene extends Scene {
     onInitialize(engine: Engine<any>): void {
         this.backgroundColor = Color.Gray
 
-        this.caseCardA = document.createElement("div") as HTMLElement
+        // this.caseCardA = document.createElement("div") as HTMLElement
+
+        // //  Definir opacidade do elemento para 1 = visível
+        // this.caseCardA.style.opacity = "1"
+
+        // // Inserir elementoTexto no container-game
+        // let containerGame = document.querySelector(".container-game") as HTMLElement
+        // containerGame.appendChild(this.caseCardA)
+
+        // // Adicionar classe na div criada (card-mesa-a)
+        // this.caseCardA.classList.add("card-mesa-a")
+
+        // // Adicionar titulo e paragrafo dentro do conteudo da div
+        // // <img src="" alt="">
+        
+        // this.caseCardA.innerHTML = `
+        // <img src="src/images/mc cantando para ganhar mc frita.jpg" alt="Foto de Mc Donalds, ganha um batata frita extra so que pedisse cantando">
+        // <p>Em 2023, o McDonald's lançou a campanha "Cantando no Drive", os clientes pode ganhavam uma batata extra ao pedir uma oferta de Big Mac, bebida e batata no drive-thru, mas só se pedisse cantando. </p>`
+    }
+
+    onActivate(context: SceneActivationContext<unknown>): void {
+        // Pegar dados vindos da cena passada 
+        this.objetoInteracao = context.data
+
+        console.log(this.objetoInteracao);
+
+        // Se for a mesa a
+        if (this.objetoInteracao.nomeDoActor == "mesa_stand_a") {
+            this.textoDaCena = "Essa é a descrição do case A"
+
+            this.caseCardA = document.createElement("div") as HTMLElement
 
         //  Definir opacidade do elemento para 1 = visível
         this.caseCardA.style.opacity = "1"
@@ -36,28 +66,56 @@ export class caseScene extends Scene {
         // <img src="" alt="">
         
         this.caseCardA.innerHTML = `
-        <p>abwydwadiyuavwdyvaidwayvdiyawdvwiayvuayvisddwaaaaaaaaaaaaaaaaaaaaaaaaaaaaduwdobiuawbduabduwadbuawbdodbwadbukwadbakwbdwuabdauldbwalbdawbjdawd wuabublaubklubwuabcuwa ljdludldwknlawhkufj</p>`
-    }
-
-    onActivate(context: SceneActivationContext<unknown>): void {
-        // Pegar dados vindos da cena passada 
-        this.objetoInteracao = context.data
-
-        console.log(this.objetoInteracao);
-
-        // Se for a mesa a
-        if (this.objetoInteracao.nomeDoActor == "mesa_stand_a") {
-            this.textoDaCena = "Essa é a descrição do case A"
+        <img src="src/images/mc cantando para ganhar mc frita.jpg" alt="Foto de Mc Donalds, ganha um batata frita extra so que pedisse cantando">
+        <p>Em 2023, o McDonald's lançou a campanha "Cantando no Drive", os clientes pode ganhavam uma batata extra ao pedir uma oferta de Big Mac, bebida e batata no drive-thru, mas só se pedisse cantando. </p>`
         }
+
+
 
         // Se for a mesa b
         if (this.objetoInteracao.nomeDoActor == "mesa_stand_b") {
             this.textoDaCena = "Essa é a descrição do case B"
+
+            this.caseCardB = document.createElement("div") as HTMLElement
+
+        //  Definir opacidade do elemento para 1 = visível
+        this.caseCardB.style.opacity = "1"
+
+        // Inserir elementoTexto no container-game
+        let containerGame = document.querySelector(".container-game") as HTMLElement
+        containerGame.appendChild(this.caseCardB)
+
+        // Adicionar classe na div criada (card-mesa-a)
+        this.caseCardB.classList.add("card-mesa-b")
+
+        // Adicionar titulo e paragrafo dentro do conteudo da div
+        
+        this.caseCardB.innerHTML = `
+    <img src="src/images/duolingo.jpg" alt="Foto de aplicativo do duolingo">
+    <p>Em 2012, criamos um aplicativo de aprendizagem de idiomas que utiliza elementos de gamificação para motivar os usuários a praticar idiomas todos os dias</p>`
         }
 
         // Se for a mesa c
         if (this.objetoInteracao.nomeDoActor == "mesa_stand_c") {
             this.textoDaCena = "Essa é a descrição do case C"
+
+            this.caseCardC = document.createElement("div") as HTMLElement
+
+        //  Definir opacidade do elemento para 1 = visível
+        this.caseCardC.style.opacity = "1"
+
+        // Inserir elementoTexto no container-game
+        let containerGame = document.querySelector(".container-game") as HTMLElement
+        containerGame.appendChild(this.caseCardC)
+
+        // Adicionar classe na div criada (card-mesa-a)
+        this.caseCardC.classList.add("card-mesa-c")
+
+        // Adicionar titulo e paragrafo dentro do conteudo da div
+        
+    //     this.caseCardC.innerHTML = `
+    // <img src="src/images/duolingo.jpg" alt="Foto de aplicativo do duolingo">
+    // <p>A Nike implementou a gamificação no seu aplicativo Nike+, que incentiva os usuários a estabelecerem metas diárias de corrida para competir uns com os outros</p>`
         }
     }
 }
